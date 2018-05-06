90616b693035e5e5d9d68a4495258032bdc15343
CaseyInHaengsin <casey.rowley89@gmail.com>
2018-05-06 13:21:35 -0600

    inserting items into database

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -2,4 +2,43 @@
 
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('')
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+
+    if(err) {
+       return console.log('Unable to connect to the database server.');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+  /*  db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+       if(err){
+           return console.log('Unable to insert todo', err);
+
+       }
+
+       console.log(JSON.stringify(result.ops));
+
+    });*/
+
+    //insert new doc into Users collection. Give it a name property, age property
+    //and a location string
+
+    db.collection('Users').insertOne({
+        name: 'Casey',
+        age: 28,
+        location: 'Salt Lake City'
+    }, (err, result) => {
+        if(err){
+            return console.log('unable to insert user', err);
+        }
+        console.log(JSON.stringify(result.ops));
+
+    })
+
+    client.close();
+
+});
