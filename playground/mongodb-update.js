0e44b22fd85db7118fdc39e1e3e0b142178c0682
Casey Rowley <casey.rowley89@gmail.com>
2018-05-08 11:08:48 -0600

    added update file and am crushing challenges

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+
+    if(err) {
+        return console.log('Unable to connect to the database server.');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+/*    db.collection('Todos').findOneAndUpdate({_id: new ObjectID('5af1d25ec75b9f037f48d9b6')},
+
+        {$set:
+                {
+                    completed: true
+                },
+
+        },
+        {returnOriginal: false}).then((result) => {
+        console.log(result);
+    });*/
+
+    db.collection('Users').findOneAndUpdate({_id: new ObjectID('5af1d8b3c75b9f037f48d9d0')},
+
+        {$set:
+                {
+                    name: 'Ben'
+                },
+
+        },
+        {returnOriginal: false}).then((result) => {
+        console.log(result);
+    });
+
+
+
+    //client.close();
+
+});
