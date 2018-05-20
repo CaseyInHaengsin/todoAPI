8b9a4e7a5e57adf315cc8a7b5c930de4e3d5c662
Casey Rowley <casey.rowley89@gmail.com>
2018-05-19 20:12:57 -0600

    added Auth token and limited visible data

--- server/server.js
+++ server/server.js
@@ -118,8 +118,12 @@ app.post('/users', (req, res) => {
      var user = new User(body);
 
 
-     user.save().then((doc) => {
-         res.send(doc);
+
+     user.save().then(() => {
+         return user.generateAuthToken();
+         //res.send(doc);
+     }).then((token) => {
+         res.header('x-auth', token).send(user);
      }).catch((e) => {
          res.status(400).send(e);
      })
