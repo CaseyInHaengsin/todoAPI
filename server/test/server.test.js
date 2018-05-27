c2af40540b267804aa9e9e0d99d17835091db7c8
Casey Rowley <casey.rowley89@gmail.com>
2018-05-26 19:02:18 -0600

    added post /users/login route and x-auth verification

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,6 +4,7 @@ const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 //const should = require('should');
 
+const {User} = require('./../models/user');
 const {Todo} = require('./../models/todo');
 
 const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
@@ -111,7 +112,7 @@ describe('DELETE /todos/:id', () => {
                }
                //query database using findById toNotExist
                Todo.findById(hexId).then((todo) => {
-                   expect(todo).toBeFalsy();
+                   expect(todo).toNotExist();
                    done();
                }).catch((e) => done(e));
             });
@@ -157,25 +158,96 @@ describe('PATCH /todos/:id', () => {
 
    });
 
-   it('should clear completedAt when todo is not completed', (done) => {
+    it('should clear completedAt when todo is not completed', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        var text = 'This should be the new text!!';
 
-       var hexID = todos[1]._id.toHexString();
-       //console.log(hexID);
-       var text = 'This should be the new text!!!';
-       //make patch request and send data
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
+            .end(done);
+    });
+});
+
+
+describe('GET /users/me', () => {
+   it('should return user if authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+               expect(res.body._id).toBe(users[0]._id.toHexString());
+               expect(res.body.email).toBe(users[0].email);
+            })
+            .end(done);
+   });
+
+   it('should return a 401 if not authenticated', (done) => {
        request(app)
-           .patch(`/todos/${hexID}`)
-           .send({
-               completed: false,
-               text
-           })
-           .expect(200)
+           .get('/users/me')
+           .expect(401)
            .expect((res) => {
-               expect(res.body.todo.text).toBe(text);
-               expect(res.body.todo.completed).toBe(false);
-               expect(res.body.todo.completedAt).toBeFalsy();
+              expect(res.body).toEqual({});
            })
            .end(done);
-
    });
+});
+
+describe('POST /users', () => {
+    it('should create a user', (done) => {
+        var email = 'example@example.com';
+        var password = '123mnb!';
+
+        request(app)
+            .post('/users')
+            .send({email, password})
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+                expect(res.body._id).toExist();
+                expect(res.body.email).toBe(email);
+            })
+            .end((err) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findOne({email}).then((user) => {
+                    expect(user).toExist();
+                    expect(user.password).toNotBe(password);
+                    done();
+                });
+            });
+    });
+    it('should return validation errors if request invalid', (done) => {
+        request(app)
+            .post('/users')
+            .send({
+                email: 'and',
+                password: '123'
+            })
+            .expect(400)
+            .end(done);
+    });
+
+    it('should not create user if email in use', (done) => {
+        request(app)
+            .post('/users')
+            .send({
+                email: users[0].email,
+                password: 'Password123!'
+            })
+            .expect(400)
+            .end(done);
+    });
 });
