2dfa5d803bf975bb9fdb1179a32e9512469ba767
Casey Rowley <casey.rowley89@gmail.com>
2018-05-26 18:55:07 -0600

    bcrypt challenge complete.

--- server/models/user.js
+++ server/models/user.js
@@ -77,6 +77,30 @@ UserSchema.statics.findByToken = function(token) {
 
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            //use bcrypt.compare to compare password and user.password
+            //if true, call resolve
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res){
+                    resolve(user);
+                }
+                else{
+                    reject();
+                }
+            });
+            //if false, call the reject method
+        });
+    })
+};
+
 UserSchema.pre('save', function(next) {
     var user = this;
 
